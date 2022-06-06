cf47efc6af19a56e726b2e257c00221676fc0780
Oguzhan Acarca <oguzhanacarca@outlook.com>
2022-06-06 23:29:21 +0300

    DaireAlanHesaplama eklendi. 06.06.2022

---
+++ src/DaireAlanHesaplama.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DaireAlanHesaplama {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        double yariCap, daireAlan, daireCevre, merkezAci, merkezAlan;
+
+        System.out.print("Yarý Çapýný giriniz: ");
+        yariCap = input.nextDouble();
+        System.out.print("Merkez Açýsýnýn Ölçüsünü Giriniz: ");
+        merkezAci = input.nextDouble();
+
+
+        // Alan Hesaplama pi*r*r
+        daireAlan = Math.PI * yariCap * yariCap;
+        daireCevre = 2 * Math.PI * yariCap;
+        merkezAlan = ((Math.PI * (yariCap*yariCap) * merkezAci) / 360);
+        System.out.println("Daire Alaný: " +daireAlan);
+        System.out.println("Daire Çevresi: " +daireCevre);
+        System.out.println("Merkez Açýsý Verilmiþ Daire Alaný: " +merkezAlan);
+
+    }
+
+}
